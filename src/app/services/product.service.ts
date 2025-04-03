ba004596176cb7e2e9f5aa71392e948a938f5ba6
LMG87 <lm.gomez.guerrero.87@gmail.com>
2025-04-02 23:47:22 -0500

    add initial components, services, and routing for cart, users, and products

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BaseHttpService } from './base-http.service';
+import { Observable } from 'rxjs';
+import { Product } from '../interfaces/store.interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService extends BaseHttpService {
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+  }
+
+  getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/products/${id}`);
+  }
+}
